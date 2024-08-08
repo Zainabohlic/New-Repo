523b9282907c5f5f567997cf6496832f0592e83e
Zainab Aslam <zainab.aslam@iep.local>
2024-08-08 12:15:40 +0500

    Initial commit

---
+++ JS/JavaScript.js
@@ -0,0 +1,11 @@
+function showLoginAlert() {
+    alert("Please login to access this feature.");
+}
+
+function showSignupAlert() {
+    alert("Signup feature is coming soon.");
+}
+
+function submitForm() {
+   alert("Thankyou for submitting your feedback :)")
+}
